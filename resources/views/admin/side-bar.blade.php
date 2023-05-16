72910c5e147f0f9f2cb627202186e4d86143571a
Kien Ho <kien.ho@tiahgroup.com>
2023-05-16 15:27:21 +0700

    Update side-bar.blade.php

--- resources/views/admin/side-bar.blade.php
+++ resources/views/admin/side-bar.blade.php
@@ -11,7 +11,7 @@
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <img src="{{ asset('image/AdminLTELogo.png') }}" alt="Admin Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">VIDEO COURSES</span>
+      <span class="brand-text font-weight-light">QUẢN TRỊ TNB</span>
     </a>
 
     <!-- Sidebar -->
