ba3af4311b188cb7233cb91f38b940c8cf55d63f
Kien Ho <kien.ho@tiahgroup.com>
2023-10-25 10:46:39 +0700

    Update site-banner.blade.php

--- resources/views/component/site-banner.blade.php
+++ resources/views/component/site-banner.blade.php
@@ -2,17 +2,17 @@
     <div class="slick-banner slick-items-custom">
         @if($layout->banner_1 != '')
         <div class="items">
-            <a href="{{ $layout->link_banner_1 }}"><img class="w-100 d-block" src="{{ asset($layout->banner_1) }}" alt=""></a>
+            <a href="{{ $layout->link_banner_1 }}" target="_blank"><img class="w-100 d-block" src="{{ asset($layout->banner_1) }}" alt=""></a>
         </div>
         @endif
         @if($layout->banner_2 != '')
         <div class="items">
-            <a href="{{ $layout->link_banner_2 }}"><img class="w-100 d-block" src="{{ asset($layout->banner_2) }}" alt=""></a>
+            <a href="{{ $layout->link_banner_2 }}" target="_blank"><img class="w-100 d-block" src="{{ asset($layout->banner_2) }}" alt=""></a>
         </div>
         @endif
         @if($layout->banner_3 != '')
         <div class="items">
-            <a href="{{ $layout->link_banner_3 }}"><img class="w-100 d-block" src="{{ asset($layout->banner_3) }}" alt=""></a>
+            <a href="{{ $layout->link_banner_3 }}" target="_blank"><img class="w-100 d-block" src="{{ asset($layout->banner_3) }}" alt=""></a>
         </div>
         @endif
     </div>
